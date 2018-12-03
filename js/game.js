8c33417a8d9819bc90dcedc9eaee5c7af33c7266
Patrick-G-W <patrick.g.ward@protonmail.com>
2018-12-03 14:04:22 +0000

    Fixed errors in game.js in preparation of additional levels

--- js/game.js
+++ js/game.js
@@ -7,7 +7,6 @@ var cursors;
 var bird;
 
 var score = 0;
-var scoreText = 'Score: 0';
 
 var springs;
 var spikes;
@@ -19,13 +18,11 @@ var door;
 var behindDoors;
 
 var point;
-var bigPoint;
 var points;
 var bigPoints;
 
 var keys;
 var keyInventory = 0;
-var inventoryText;
 
 var soundForest;
 var soundFootstep;
@@ -35,95 +32,91 @@ var soundWin;
 var soundDoorLock;
 var soundDeath;
 
-var restarting;
-
-var stars;
-
-var Game = {
+var Game = { // incorrect error, is used by other js files through states. without game, other js cannot tell what this file is
 	preload: function () {
-        game.load.image('sky', 'assets/sky.png');
-        game.load.image('background', 'assets/background.png'); //https://jesse-m.itch.io/jungle-pack
-        game.load.image('ground', 'assets/ground.png');
-        game.load.image('newGround', 'assets/newGround.png');
-        game.load.image('platform', 'assets/platform.png');
-        game.load.image('springs', 'assets/jump.png'); //http://pixelartmaker.com/art/4dae9891e38493b
-        game.load.image('spikes', 'assets/spike.png');
-        game.load.image('spikesRight', 'assets/spikeRightSide.png');
-        game.load.image('spikesLeft', 'assets/spikeLeftSide.png');
-        game.load.image('key', 'assets/key.png');
-        game.load.image('door', 'assets/door.png'); //https://opengameart.org/content/wooden-fortress-and-animated-doors
-        game.load.image('point', 'assets/point.png');
-        game.load.image('bigPoint', 'assets/bigPoint.png');
-        game.load.image('behindDoor', 'assets/behindDoor.png');
-        game.load.image('blood', 'assets/blood.png');
-        game.load.image('gameOver', 'assets/gameOver.png');
-        game.load.image('mediumButton', 'assets/mediumButton.png');
-        game.load.audio('soundForest', 'assets/forest.ogg'); //https://freesound.org/people/VKProduktion/sounds/231537/
-        game.load.audio('soundFootstep', 'assets/footstep.ogg'); //https://freesound.org/people/LittleRobotSoundFactory/sounds/270414/
-        game.load.audio('soundPoint', 'assets/point.ogg'); //https://freesound.org/people/LittleRobotSoundFactory/sounds/270408/
-        game.load.audio('soundKey', 'assets/key.ogg'); //https://freesound.org/people/LittleRobotSoundFactory/sounds/270404/
-        game.load.audio('soundWin', 'assets/win.ogg'); //https://freesound.org/people/LittleRobotSoundFactory/sounds/270402/
-        game.load.audio('soundDoorLock', 'assets/doorLock.ogg'); //https://freesound.org/people/MrAuralization/sounds/158626/
-        game.load.audio('soundDeath', 'assets/death.ogg'); //https://freesound.org/people/DrMinky/sounds/167074/
-        game.load.spritesheet('dude', 'assets/spritesheet.png', 23, 34);
-        game.load.spritesheet('bird', 'assets/bird.png', 32, 32);
-        game.load.spritesheet('doorOpening', 'assets/doorOpening.png', 64, 64);
-        game.load.bitmapFont('8bitWonder', 'assets/8bitWonder.png', 'assets/8bitWonder.fnt');
+        this.game.load.image('sky', 'assets/sky.png');
+        this.game.load.image('background', 'assets/background.png'); //https://jesse-m.itch.io/jungle-pack
+        this.game.load.image('ground', 'assets/ground.png');
+        this.game.load.image('newGround', 'assets/newGround.png');
+        this.game.load.image('platform', 'assets/platform.png');
+        this.game.load.image('springs', 'assets/jump.png'); //http://pixelartmaker.com/art/4dae9891e38493b
+        this.game.load.image('spikes', 'assets/spike.png');
+        this.game.load.image('spikesRight', 'assets/spikeRightSide.png');
+        this.game.load.image('spikesLeft', 'assets/spikeLeftSide.png');
+        this.game.load.image('key', 'assets/key.png');
+        this.game.load.image('door', 'assets/door.png'); //https://opengameart.org/content/wooden-fortress-and-animated-doors
+        this.game.load.image('point', 'assets/point.png');
+        this.game.load.image('bigPoint', 'assets/bigPoint.png');
+        this.game.load.image('behindDoor', 'assets/behindDoor.png');
+        this.game.load.image('blood', 'assets/blood.png');
+        this.game.load.image('gameOver', 'assets/gameOver.png');
+        this.game.load.image('mediumButton', 'assets/mediumButton.png');
+        this.game.load.audio('soundForest', 'assets/forest.ogg'); //https://freesound.org/people/VKProduktion/sounds/231537/
+        this.game.load.audio('soundFootstep', 'assets/footstep.ogg'); //https://freesound.org/people/LittleRobotSoundFactory/sounds/270414/
+        this.game.load.audio('soundPoint', 'assets/point.ogg'); //https://freesound.org/people/LittleRobotSoundFactory/sounds/270408/
+        this.game.load.audio('soundKey', 'assets/key.ogg'); //https://freesound.org/people/LittleRobotSoundFactory/sounds/270404/
+        this.game.load.audio('soundWin', 'assets/win.ogg'); //https://freesound.org/people/LittleRobotSoundFactory/sounds/270402/
+        this.game.load.audio('soundDoorLock', 'assets/doorLock.ogg'); //https://freesound.org/people/MrAuralization/sounds/158626/
+        this.game.load.audio('soundDeath', 'assets/death.ogg'); //https://freesound.org/people/DrMinky/sounds/167074/
+        this.game.load.spritesheet('dude', 'assets/spritesheet.png', 23, 34);
+        this.game.load.spritesheet('bird', 'assets/bird.png', 32, 32);
+        this.game.load.spritesheet('doorOpening', 'assets/doorOpening.png', 64, 64);
+        this.game.load.bitmapFont('8bitWonder', 'assets/8bitWonder.png', 'assets/8bitWonder.fnt');
     },
 
 	create: function () {
-        game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        soundForest = game.add.audio('soundForest');
-        soundFootstep = game.add.audio('soundFootstep');
-        soundPoint = game.add.audio('soundPoint');
-        soundKey = game.add.audio('soundKey');
-        soundWin = game.add.audio('soundWin');
-        soundDoorLock = game.add.audio('soundDoorLock');
-        soundDeath = game.add.audio('soundDeath');
+        soundForest = this.game.add.audio('soundForest');
+        soundFootstep = this.game.add.audio('soundFootstep');
+        soundPoint = this.game.add.audio('soundPoint');
+        soundKey = this.game.add.audio('soundKey');
+        soundWin = this.game.add.audio('soundWin');
+        soundDoorLock = this.game.add.audio('soundDoorLock');
+        soundDeath = this.game.add.audio('soundDeath');
 
         if (sessionStorage.getItem('music') === 'true') {
-            game.time.events.add(Phaser.Timer.SECOND, this.musicDelay, this);
+            this.game.time.events.add(Phaser.Timer.SECOND, this.musicDelay, this);
         }
 
-        var background = game.add.sprite(0, 0, 'background');
+        var background = this.game.add.sprite(0, 0, 'background');
 
-        background.height = game.height;
-        background.width = game.width;
+        background.height = this.game.height;
+        background.width = this.game.width;
 
-        this.inventoryText = game.add.bitmapText(10, 10, '8bitWonder', 'Inventory ', 20);
+        this.inventoryText = this.game.add.bitmapText(10, 10, '8bitWonder', 'Inventory ', 20);
 
 
-        behindDoors = game.add.group();
+        behindDoors = this.game.add.group();
         var behindDoor = behindDoors.create(720, 465, 'behindDoor');
 
         behindDoor.scale.setTo(1, 1);
 
-        platforms = game.add.group();
+        platforms = this.game.add.group();
 
         platforms.enableBody = true;
 
-        springs = game.add.group();
+        springs = this.game.add.group();
 
         springs.enableBody = true;
 
-        spikes = game.add.group();
+        spikes = this.game.add.group();
 
         spikes.enableBody = true;
 
-        spikesRight = game.add.group();
+        spikesRight = this.game.add.group();
 
         spikesRight.enableBody = true;
 
-        spikesLeft = game.add.group();
+        spikesLeft = this.game.add.group();
 
         spikesLeft.enableBody = true;
 
-        spikesRight = game.add.group();
+        spikesRight = this.game.add.group();
 
         spikesRight.enableBody = true;
 
-        keys = game.add.group();
+        keys = this.game.add.group();
 
         keys.enableBody = true;
 
@@ -133,7 +126,7 @@ var Game = {
 
         key.body.bounce.y = 1;
 
-        doors = game.add.group();
+        doors = this.game.add.group();
 
         doors.enableBody = true;
 
@@ -145,9 +138,9 @@ var Game = {
 
         door.frame = 1;
 
-        this.scoreText = game.add.bitmapText(600, 10, '8bitWonder', 'Score 0', 20);
+        this.scoreText = this.game.add.bitmapText(600, 10, '8bitWonder', 'Score 0', 20);
 
-        var ground = platforms.create(0, game.world.height - 64, 'newGround');
+        var ground = platforms.create(0, this.game.world.height - 64, 'newGround');
 
         ground.body.immovable = true;
         ground.body.setSize(800, 100, 0, 5);
@@ -170,19 +163,19 @@ var Game = {
         ledge.width = 300;
         ledge.body.setSize(400, 30, 0, 5);
 
-        bird = game.add.sprite(-50, 30, 'bird');
+        bird = this.game.add.sprite(-50, 30, 'bird');
 
-        game.physics.arcade.enable(bird);
+        this.game.physics.arcade.enable(bird);
 
         bird.animations.add('right', [0, 1, 2, 3], 10, true);
 
         bird.animations.play('right');
 
-        game.add.tween(bird).to({x: 850}, 10000, Phaser.Easing.Quadratic.InOut, true, 5000, 1000, false);
+        this.game.add.tween(bird).to({x: 850}, 10000, Phaser.Easing.Quadratic.InOut, true, 5000, 1000, false);
 
-        player = game.add.sprite(32, game.world.height - 150, 'dude');
+        player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
 
-        game.physics.arcade.enable(player);
+        this.game.physics.arcade.enable(player);
 
         player.body.gravity.y = 350;
         player.body.collideWorldBounds = true;
@@ -191,7 +184,7 @@ var Game = {
         player.animations.add('left', [8, 9, 10, 11, 12, 13, 14, 15], 10, true);
         player.animations.add('right', [0, 1, 2, 3, 4, 5, 6, 7], 10, true);
 
-        cursors = game.input.keyboard.createCursorKeys();
+        cursors = this.game.input.keyboard.createCursorKeys();
 
 
 
@@ -216,20 +209,20 @@ var Game = {
     },
 
 	update: function () {
-        var hitPlatform = game.physics.arcade.collide(player, platforms);
-        var hitSpring = game.physics.arcade.collide(player, springs);
-        var hitSpike = game.physics.arcade.collide(player, spikes);
-        var hitSpikeLeft = game.physics.arcade.collide(player, spikesLeft);
-        var hitSpikeRight = game.physics.arcade.collide(player, spikesRight);
-        var hitKey = game.physics.arcade.collide(player, keys);
-        var keyHitPlatform = game.physics.arcade.collide(keys, platforms);
-        var hitDoor = game.physics.arcade.collide(player, doors);
+        var hitPlatform = this.game.physics.arcade.collide(player, platforms);
+        var hitSpring = this.game.physics.arcade.collide(player, springs);
+        var hitSpike = this.game.physics.arcade.collide(player, spikes);
+        var hitSpikeLeft = this.game.physics.arcade.collide(player, spikesLeft);
+        var hitSpikeRight = this.game.physics.arcade.collide(player, spikesRight);
+        var hitKey = this.game.physics.arcade.collide(player, keys);
+        this.game.physics.arcade.collide(keys, platforms);
+        var hitDoor = this.game.physics.arcade.collide(player, doors);
 
-        game.physics.arcade.collide(points, platforms);
-        game.physics.arcade.collide(bigPoints, platforms);
+        this.game.physics.arcade.collide(points, platforms);
+        this.game.physics.arcade.collide(bigPoints, platforms);
 
-        game.physics.arcade.overlap(player, points, this.collectPoint, null, this);
-        game.physics.arcade.overlap(player, bigPoints, this.collectBigPoint, null, this);
+        this.game.physics.arcade.overlap(player, points, this.collectPoint, null, this);
+        this.game.physics.arcade.overlap(player, bigPoints, this.collectBigPoint, null, this);
 
         player.body.velocity.x = 0;
 
@@ -277,24 +270,24 @@ var Game = {
         }
 
         if (hitKey) {
-            game.add.text(game.world.centerX, game.world.centerY, 'You collected a key!').lifespan = 1000;
+            this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'You collected a key!').lifespan = 1000;
             keys.destroy();
             if (sessionStorage.getItem('soundEffect') === 'true') {
                 soundKey.play();
             }
-            game.add.sprite(190, 10, 'key');
+            this.game.add.sprite(190, 10, 'key');
             keyInventory = 1;
         }
 
         if (hitDoor && keyInventory === 1) {
-            game.add.text(game.world.centerX, game.world.centerY, 'Level complete');
+            this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'Level complete');
             door.animations.play('door');
             if (sessionStorage.getItem('soundEffect') === 'true') {
                 soundWin.play('', 0, 1, true, false);
             }
         }
         else if (hitDoor && keyInventory === 0) {
-            game.add.text(game.world.centerX, game.world.centerY, 'You need to collect the key first').lifespan = 1000;
+            this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'You need to collect the key first').lifespan = 1000;
             if (sessionStorage.getItem('soundEffect') === 'true') {
                 soundDoorLock.play();
             }
@@ -302,7 +295,7 @@ var Game = {
     },
 
 	generatePoints: function () {
-		points = game.add.group();
+		points = this.game.add.group();
 		points.enableBody = true;
 
 		var point = points.create(140, 500, 'point');
@@ -320,10 +313,10 @@ var Game = {
 	},
 
 	generateBigPoints: function () {
-		bigPoints = game.add.group();
+		bigPoints = this.game.add.group();
 		bigPoints.enableBody = true;
 
-		var bigPoint = bigPoints.create(700, 160, 'bigPoint');
+		bigPoints.create(700, 160, 'bigPoint');
 	},
 
 	collectPoint: function (player, point) {
@@ -354,22 +347,22 @@ var Game = {
     },
 
 	death: function () {
-        game.add.image(game.world.centerX - 200, game.world.centerY - 134, 'gameOver').alpha = 0.6;
+        this.game.add.image(this.game.world.centerX - 200, this.game.world.centerY - 134, 'gameOver').alpha = 0.6;
 
-        game.add.bitmapText(game.world.centerX - 180, game.world.centerY - 70, '8bitWonder', 'GAME OVER', 40);
+        this.game.add.bitmapText(this.game.world.centerX - 180, this.game.world.centerY - 70, '8bitWonder', 'GAME OVER', 40);
 
-        game.add.button(game.world.centerX - 150, game.world.centerY + 20, 'mediumButton', this.restart, this);
-        game.add.bitmapText(game.world.centerX - 135, game.world.centerY + 50, '8bitWonder', 'Restart', 15);
-        game.add.button(game.world.centerX + 20, game.world.centerY + 20, 'mediumButton', this.backToMenu, this);
-        game.add.bitmapText(game.world.centerX + 35, game.world.centerY + 40, '8bitWonder', 'Quit', 30);
-        game.add.bitmapText(game.world.centerX + 35, game.world.centerY + 40, '8bitWonder', 'Quit', 30);
-        game.add.bitmapText(game.world.centerX - 75, game.world.centerY - 20, '8bitWonder', this.scoreText.text, 20);
+        this.game.add.button(this.game.world.centerX - 150, this.game.world.centerY + 20, 'mediumButton', this.restart, this);
+        this.game.add.bitmapText(this.game.world.centerX - 135, this.game.world.centerY + 50, '8bitWonder', 'Restart', 15);
+        this.game.add.button(this.game.world.centerX + 20, this.game.world.centerY + 20, 'mediumButton', this.backToMenu, this);
+        this.game.add.bitmapText(this.game.world.centerX + 35, this.game.world.centerY + 40, '8bitWonder', 'Quit', 30);
+        this.game.add.bitmapText(this.game.world.centerX + 35, this.game.world.centerY + 40, '8bitWonder', 'Quit', 30);
+        this.game.add.bitmapText(this.game.world.centerX - 75, this.game.world.centerY - 20, '8bitWonder', this.scoreText.text, 20);
         this.scoreText.kill();
 
         if (sessionStorage.getItem('soundEffect') === 'true') {
             soundDeath.play();
         }
-        var emitter = game.add.emitter(0, 0, 1000);
+        var emitter = this.game.add.emitter(0, 0, 1000);
         emitter.makeParticles('blood');
         emitter.gravity = 200;
         emitter.x = player.x;
@@ -418,13 +411,13 @@ var Game = {
     },
 
 	render: function () {
-        //game.debug.body(player);
-        //game.debug.physicsGroup(spikes);
-        //game.debug.physicsGroup(springs);
-        //game.debug.physicsGroup(spikesLeft);
-        //game.debug.physicsGroup(spikesRight);
-        //game.debug.physicsGroup(keys);
-        //game.debug.physicsGroup(springs);
-        //game.debug.physicsGroup(platforms);
+        //this.game.debug.body(player);
+        //this.game.debug.physicsGroup(spikes);
+        //this.game.debug.physicsGroup(springs);
+        //this.game.debug.physicsGroup(spikesLeft);
+        //this.game.debug.physicsGroup(spikesRight);
+        //this.game.debug.physicsGroup(keys);
+        //this.game.debug.physicsGroup(springs);
+        //this.game.debug.physicsGroup(platforms);
     }
 };
