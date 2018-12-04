6ad75d5cae48493f4562b68ccd1829e39a828ffb
Patrick-G-W <patrick.g.ward@protonmail.com>
2018-12-04 10:14:55 +0000

    Next level completed, need to make accessible to first level after completion

--- js/game2.js
+++ js/game2.js
@@ -1,4 +1,4 @@
-console.log('Game loaded');
+console.log('Game2 loaded');
 
 var player;
 var platforms;
@@ -32,8 +32,8 @@ var soundWin;
 var soundDoorLock;
 var soundDeath;
 
-var Game = { // incorrect error, is used by other js files through states. without game, other js cannot tell what this file is
-	preload: function () {
+var Game2 = { // incorrect error, is used by other js files through states. without game, other js cannot tell what this file is
+    preload: function () {
         this.game.load.image('sky', 'assets/sky.png');
         this.game.load.image('background', 'assets/background.png'); //https://jesse-m.itch.io/jungle-pack
         this.game.load.image('ground', 'assets/ground.png');
@@ -64,7 +64,7 @@ var Game = { // incorrect error, is used by other js files through states. witho
         this.game.load.bitmapFont('8bitWonder', 'assets/8bitWonder.png', 'assets/8bitWonder.fnt');
     },
 
-	create: function () {
+    create: function () {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
         soundForest = this.game.add.audio('soundForest');
@@ -88,7 +88,7 @@ var Game = { // incorrect error, is used by other js files through states. witho
 
 
         behindDoors = this.game.add.group();
-        var behindDoor = behindDoors.create(720, 465, 'behindDoor');
+        var behindDoor = behindDoors.create(0, 465, 'behindDoor');
 
         behindDoor.scale.setTo(1, 1);
 
@@ -120,7 +120,7 @@ var Game = { // incorrect error, is used by other js files through states. witho
 
         keys.enableBody = true;
 
-        var key = keys.create(10, 150, 'key');
+        var key = keys.create(330, 150, 'key');
 
         key.body.gravity.y = 100;
 
@@ -130,7 +130,7 @@ var Game = { // incorrect error, is used by other js files through states. witho
 
         doors.enableBody = true;
 
-        door = doors.create(730, 475, 'doorOpening');
+        door = doors.create(8, 475, 'doorOpening');
 
         door.animations.add('door', [1, 2, 3, 4], 3, false);
 
@@ -145,23 +145,28 @@ var Game = { // incorrect error, is used by other js files through states. witho
         ground.body.immovable = true;
         ground.body.setSize(800, 100, 0, 5);
 
-        var ledge = platforms.create(400, 400, 'platform');
+        var ledge = platforms.create(- 10, 400, 'platform');
         ledge.body.immovable = true;
 
         ledge.width = 400;
-        ledge.body.setSize(400, 30, 0, 5);
+        ledge.body.setSize(375, 30, 0, 5);
 
-        ledge = platforms.create(-150, 250, 'platform');
+        ledge = platforms.create(300, 300, 'platform');
         ledge.body.immovable = true;
 
-        ledge.width = 400;
-        ledge.body.setSize(370, 30, 0, 5);
+        ledge.width = 600;
+        ledge.body.setSize(450, 30, 0, 5);
 
-        ledge = platforms.create(600, 210, 'platform');
+        ledge = platforms.create(0, 210, 'platform');
         ledge.body.immovable = true;
 
         ledge.width = 300;
-        ledge.body.setSize(400, 30, 0, 5);
+        ledge.body.setSize(375, 30, 0, 5);
+
+        ledge = platforms.create(0, 120, 'platform');
+        ledge.body.immovable = true;
+
+        ledge.width = 200;
 
         bird = this.game.add.sprite(-50, 30, 'bird');
 
@@ -173,7 +178,7 @@ var Game = { // incorrect error, is used by other js files through states. witho
 
         this.game.add.tween(bird).to({x: 850}, 10000, Phaser.Easing.Quadratic.InOut, true, 5000, 1000, false);
 
-        player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
+        player = this.game.add.sprite(10, 10, 'dude');
 
         this.game.physics.arcade.enable(player);
 
@@ -187,28 +192,28 @@ var Game = { // incorrect error, is used by other js files through states. witho
         cursors = this.game.input.keyboard.createCursorKeys();
 
 
+        this.placeSpring(390, 285);
 
-        this.placeSpring(360, 520);
+        this.placeSpike(270, 190);
+        this.placeSpike(330, 277);
+        this.placeSpike(500, 277);
+        this.placeSpike(250, 377);
+        this.placeSpike(150, 377);
+        this.placeSpike(150, 512);
+        this.placeSpike(250, 512);
+        this.placeSpike(350, 512);
+        this.placeSpike(550, 512);
+        this.placeSpike(650, 512);
 
-        this.placeSpring(440, 380);
+        this.placeRightSpike(200,120);
 
-        this.placeSpike(300, 515);
+        this.placeRightSpike(390, 400);
 
-        this.placeSpike(200, 515);
-
-        this.placeSpike(400, 375);
-
-        this.placeLeftSpike(370, 400);
-
-        this.placeLeftSpike(570, 210);
-
-        this.placeRightSpike(250, 250);
-
-		this.generatePoints();
-		this.generateBigPoints();
+        this.generatePoints();
+        this.generateBigPoints();
     },
 
-	update: function () {
+    update: function () {
         var hitPlatform = this.game.physics.arcade.collide(player, platforms);
         var hitSpring = this.game.physics.arcade.collide(player, springs);
         var hitSpike = this.game.physics.arcade.collide(player, spikes);
@@ -280,7 +285,11 @@ var Game = { // incorrect error, is used by other js files through states. witho
         }
 
         if (hitDoor && keyInventory === 1) {
-            this.levelComplete();
+            this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'Level complete');
+            door.animations.play('door');
+            if (sessionStorage.getItem('soundEffect') === 'true') {
+                soundWin.play('', 0, 1, true, false);
+            }
         }
         else if (hitDoor && keyInventory === 0) {
             this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'You need to collect the key first').lifespan = 1000;
@@ -290,46 +299,41 @@ var Game = { // incorrect error, is used by other js files through states. witho
         }
     },
 
-	generatePoints: function () {
-		points = this.game.add.group();
-		points.enableBody = true;
+    generatePoints: function () {
+        points = this.game.add.group();
+        points.enableBody = true;
 
-		var point = points.create(140, 500, 'point');
-		point = points.create(210, 490, 'point');
-		point = points.create(310, 490, 'point');
-		point = points.create(330, 370, 'point');
-		point = points.create(370, 300, 'point');
-		point = points.create(450, 340, 'point');
-		point = points.create(550, 360, 'point');
-		point = points.create(620, 360, 'point');
-		point = points.create(690, 360, 'point');
-		point = points.create(650, 170, 'point');
-		point = points.create(150, 210, 'point');
-		point = points.create(100, 210, 'point');
-	},
-
-	generateBigPoints: function () {
-		bigPoints = this.game.add.group();
-		bigPoints.enableBody = true;
-
-		bigPoints.create(700, 160, 'bigPoint');
-	},
-
-	collectPoint: function (player, point) {
-	    if (sessionStorage.getItem('soundEffect') === 'true') {
-            soundPoint.play();
-        }
-		point.kill();
-		score += 10;
-		this.scoreText.text = 'Score: ' + score;
-	},
-
-	musicDelay: function () {
-		soundForest.play('', 0, 0.5, true, false);
+        var point = points.create(100, 175, 'point');
+        point = points.create(600, 265, 'point');
+        point = points.create(675, 265, 'point');
+        point = points.create(100, 360, 'point');
+        point = points.create(40, 360, 'point');
+        point = points.create(450, 500, 'point');
+        point = points.create(600, 500, 'point');
+        point = points.create(700, 500, 'point');
     },
 
-	collectBigPoint: function (player, bigPoint) {
-	    if (sessionStorage.getItem('soundEffect') === 'true') {
+    generateBigPoints: function () {
+        bigPoints = this.game.add.group();
+        bigPoints.enableBody = true;
+        bigPoints.create(40, 160, 'bigPoint');
+    },
+
+    collectPoint: function (player, point) {
+        if (sessionStorage.getItem('soundEffect') === 'true') {
+            soundPoint.play();
+        }
+        point.kill();
+        score += 10;
+        this.scoreText.text = 'Score: ' + score;
+    },
+
+    musicDelay: function () {
+        soundForest.play('', 0, 0.5, true, false);
+    },
+
+    collectBigPoint: function (player, bigPoint) {
+        if (sessionStorage.getItem('soundEffect') === 'true') {
             soundPoint.play();
         }
         bigPoint.kill();
@@ -337,12 +341,12 @@ var Game = { // incorrect error, is used by other js files through states. witho
         this.scoreText.text = 'Score: ' + score;
     },
 
-	placePoint: function (x, y) {
+    placePoint: function (x, y) {
         point = points.create(x, y, 'point');
         point.body.immovable = true;
     },
 
-	death: function () {
+    death: function () {
         this.game.add.image(this.game.world.centerX - 200, this.game.world.centerY - 134, 'gameOver').alpha = 0.6;
 
         this.game.add.bitmapText(this.game.world.centerX - 180, this.game.world.centerY - 70, '8bitWonder', 'GAME OVER', 40);
@@ -370,43 +374,43 @@ var Game = { // incorrect error, is used by other js files through states. witho
         }
     },
 
-	placeSpike: function (x, y) {
+    placeSpike: function (x, y) {
         var name = spikes.create(x, y, 'spikes');
         name.body.setSize(10, 30, 10);
         name.body.immovable = true;
     },
 
-	placeLeftSpike: function (x, y) {
+    placeLeftSpike: function (x, y) {
         var name = spikesLeft.create(x, y, 'spikesLeft');
         name.body.setSize(30, 10, 0, 10);
         name.body.immovable = true;
     },
 
-	placeRightSpike: function (x, y) {
+    placeRightSpike: function (x, y) {
         var name = spikesRight.create(x, y, 'spikesRight');
         name.body.setSize(30, 10, 0, 10);
         name.body.immovable = true;
     },
 
-	placeSpring: function (x, y) {
+    placeSpring: function (x, y) {
         var name = springs.create(x, y, 'springs');
         name.body.immovable = true;
     },
 
-	restart: function () {
+    restart: function () {
         keyInventory = 0;
         score = 0;
         this.game.state.restart();
     },
 
     backToMenu: function () {
-	    if (sessionStorage.getItem('soundEffect') === 'true') {
-	        soundForest.stop();
+        if (sessionStorage.getItem('soundEffect') === 'true') {
+            soundForest.stop();
         }
-	    this.state.start('Menu');
+        this.state.start('Menu');
     },
 
-	render: function () {
+    render: function () {
         //this.game.debug.body(player);
         //this.game.debug.physicsGroup(spikes);
         //this.game.debug.physicsGroup(springs);
@@ -415,14 +419,5 @@ var Game = { // incorrect error, is used by other js files through states. witho
         //this.game.debug.physicsGroup(keys);
         //this.game.debug.physicsGroup(springs);
         //this.game.debug.physicsGroup(platforms);
-    },
-
-    levelComplete: function () {
-        this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'Level complete');
-        door.animations.play('door');
-        if (sessionStorage.getItem('soundEffect') === 'true') {
-            soundWin.play('', 0, 1, true, false);
-        }
-        this.state.start('Game2');
     }
 };
--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 
 game.state.add('Menu', Menu);
 game.state.add('Game', Game);
+game.state.add('Game2', Game2);
 game.state.add('Options', Options);
 
 game.state.start('Menu');
